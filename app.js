966fc44c6e1825ef4eaed1b23a06086873ee27fc
Sean Kelly <seankwebdev@gmail.com>
2022-08-08 21:59:05 +0100

    Set up initial backend with connection to MongoDB

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env'})
+connectDB()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, console.log(`Server running on Port ${PORT} in ${process.env.NODE_ENV} mode`))
+
+
