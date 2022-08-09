15c45459ec1c1490e46b643f8114f7e2e571e79c
Sean Kelly <seankwebdev@gmail.com>
2022-08-09 23:08:23 +0100

    Added handlebars and routing to render hbs templates

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const { engine } = require("express-handlebars")
 const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env'})
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.engine(
+    "hbs",
+    engine({
+      extname: "hbs",
+      defaultLayout: false,
+      layoutsDir: "views/layouts/",
+    })
+  );
+app.set("view engine", "hbs")
+app.set("views", "./views")
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/index'))
+
 const PORT = process.env.PORT || 3000
 
 
