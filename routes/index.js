adbddf16264333d51240bce82230fdc810604ca8
Sean Kelly <seankwebdev@gmail.com>
2022-08-14 21:58:27 +0100

    Added parameters from auth middleware to routes and passed in first name as an object for the dashboard page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     })
 })
 
-router.get('/dashboard', (req, res) => {
-    res.render('layouts/dashboard')
+router.get('/dashboard', ensureAuth, (req, res) => {
+    res.render('dashboard', {
+        name: req.user.firstName,
+    })
 })
 
 
