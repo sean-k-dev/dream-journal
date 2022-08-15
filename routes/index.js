7b3e5ef49263583ce6c7668d2249fe5111710601
Sean Kelly <seankwebdev@gmail.com>
2022-08-15 20:08:09 +0100

    Added read request for looking up dream info from MongoDB and pulling it into the DOM

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,25 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Dream = require('../models/Dream')
+
 router.get('/', ensureGuest, (req, res) => {
     res.render('login', {
         layout: 'login'
     })
 })
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const dreams = await Dream.find({ user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            dreams
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
 })
 
 
